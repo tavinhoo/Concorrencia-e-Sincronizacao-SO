d3f46483ab2c1e6b07bd2162a1891b0d791f132c
Otávio <luizotaviomtbs@gmail.com>
2024-10-10 12:35:33 -0300

    Projeto base.
    @Luiz - Primeiro Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+ 
+	+-------------+
+	|    Autores  |  
+	+-------------+
+	@Kawan 
+	@Luiz  
+	
+*/
+
+int main(int argc, char** argv) {
+	
+	// Nosso código aqui...
+	
+	return 0;
+}
